24e6c880ec959ccba2b96e67a45a780ccf1899cf
Neel Patil <neel.rp21@gmail.com>
2025-08-20 18:43:14 -0700

    Add files via upload

---
+++ widget.py
@@ -0,0 +1,10 @@
+from tkinter import Tk #importing tk from tkinter module
+
+#Create Window
+window = Tk()
+
+#Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+
+window.mainloop()
